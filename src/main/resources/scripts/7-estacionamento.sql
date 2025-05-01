b563049154af91b9d13dc865cdbf0162265930f9
Rafael <rafaasigoli1@gmail.com>
2025-04-30 21:43:59 -0300

    adicionando sql com fk

--- src/main/resources/scripts/7-estacionamento.sql
+++ src/main/resources/scripts/7-estacionamento.sql
@@ -6,13 +6,17 @@ DROP TABLE estacionamento CASCADE CONSTRAINTS;
 CREATE TABLE estacionamento (
     id NUMBER PRIMARY KEY,
     total_vagas NUMBER NOT NULL,
+    funcionario_id NUMBER,
     vagas_ocupadas NUMBER DEFAULT 0,
     preco_por_hora NUMBER NOT NULL
+    CONSTRAINT fk_est_func FOREIGN KEY (funcionario_id) REFERENCES funcionario(id)
 );
 
 CREATE TABLE veiculo (
     placa VARCHAR2(10) PRIMARY KEY,
+    cliente_id NUMBER,
     modelo VARCHAR2(50) NOT NULL,
     horas_estacionado NUMBER NOT NULL,
-    estacionamento_id NUMBER REFERENCES estacionamento(id)
+    CONSTRAINT fk_vei_est FOREIGN KEY (estacionamento_id) REFERENCES estacionamento(id),
+    CONSTRAINT fk_vei_cli FOREIGN KEY (cliente_id) REFERENCES cliente(id)
 );
