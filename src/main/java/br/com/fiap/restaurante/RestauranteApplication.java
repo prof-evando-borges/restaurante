546e5ef9a37c7d70a2c65f1ed5f670abfafae086
prof-evando <profevando.borges@fiap.com.br>
2025-05-01 09:36:21 -0300

    Exemplo de controller

--- src/main/java/br/com/fiap/restaurante/RestauranteApplication.java
+++ src/main/java/br/com/fiap/restaurante/RestauranteApplication.java
@@ -14,28 +14,8 @@ import java.util.ArrayList;
 public class RestauranteApplication {
 
     public static void main(String[] args) throws SQLException {
-        //SpringApplication.run(RestauranteApplication.class, args);
+        SpringApplication.run(RestauranteApplication.class, args);
 
-        /*Delivery delivery = new Delivery(1, "Teste");
-
-        delivery.buscarPedidos();
-
-        for (int i = 0; i < delivery.getPedidos().size(); i++) {
-            Pedido pedido = delivery.getPedidos().get(i);
-            System.out.println(pedido.getId() + " " + pedido.getStatus());
-        }*/
-
-        Pedido pedido = new Pedido(1, 1, 0, new ArrayList<Produto>(), new ArrayList<Cardapio>());
-
-        pedido.fazerPedido();
-
-        System.out.println(pedido.getId());
-
-        System.out.println(pedido.calculaTotal());
-
-        pedido.verificarStatus();
-
-        System.out.println(pedido.getStatus());
     }
 
 }
